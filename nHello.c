fdb1b52c22452808a880e6f923a903c6d8ceb89b
Guhan <35652560+Guhantg@users.noreply.github.com>
2018-01-26 16:09:17 +0530

    Create nHello.c

---
+++ nHello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i;
+    clrscr();
+    printf("Enter The Value for N\n");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    printf("\n hello");
+    getch();
+}
