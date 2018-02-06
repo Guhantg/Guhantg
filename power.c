d05d3fec12a5366be0446a5783bfe7160b54694f
Guhan <35652560+Guhantg@users.noreply.github.com>
2018-02-06 16:03:44 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter The Number\n");
+    scanf("%d",&a);
+    printf("Enter The Power\n");
+    scanf("%d",&b);
+    c=pow(a,b);
+    printf("The Value Is %d",c);
+    getch();
+}
