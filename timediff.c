810d922e833cb268f11f6f2497ce1e1bd688eb50
Guhan <35652560+Guhantg@users.noreply.github.com>
2018-02-20 15:51:50 +0530

    Create timediff.c

---
+++ timediff.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d;
+    printf("Enter First Time");
+    scanf("%d %d",&a,&b);
+    printf("Enter Second Time");
+    scanf("%d %d",&c,&d);
+    printf("%d %d",a-c,b-d);
+}
