584c68cdb665e382899fef6bc8630f24dd17c092
Guhan <35652560+Guhantg@users.noreply.github.com>
2018-02-20 16:27:09 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+ int a,i,max;
+ scanf("%d",&a);
+ int b[a];
+ for(i=0;i<a;i++)
+ scanf("%d",&b[i]);
+ max=b[0];
+ for(i=1;i<a;i++)
+ {
+   if(max<b[i])
+   max=b[i];
+ }
+ printf("%d",max);
+}
