655a1ebcc8a974463cca41110bb2cff42c4f98e2
Guhan <35652560+Guhantg@users.noreply.github.com>
2018-03-08 16:43:00 +0530

    Create 50 2 power.c

---
+++ 50 2 power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter The Number\n");
+    scanf("%d",&a);
+    b=a%2;
+    if(b==0)
+    {   printf("Yes");  }
+    else { printf("No");  }
+}
