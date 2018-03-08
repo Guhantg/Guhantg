10bc03f044cfed9f21c8d46a1cc64e5802773097
Guhan <35652560+Guhantg@users.noreply.github.com>
2018-03-08 16:21:49 +0530

    Update 28 Index.c

--- 28 Index.c
+++ 28 Index.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 void main()
 {
-    int a[10],i,n;
-    printf("Enter the limit");
-    scanf("%d",&n);
-    printf("Enter the element");
-    for(i=0;i<n;i++)
-     {
-       scanf("%d",&a[i]);
-     }
-    printf("The Elements with index\n");
-    for(i=0;i<n;i++)
-     {
-       printf("%d %d\n",i,a[i]);
-     }
+    int a[10],b,i;
+    printf("Enter The Limits");
+    scanf("%d",&b);
+    printf("Enter The Numbers");
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<b;i++)
+    {
+    printf(" %d  %d\n",i,a[i]);
+    }
 }
